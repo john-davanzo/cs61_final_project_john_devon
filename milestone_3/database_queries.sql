92e796e710057517af6e9b0697bed549b61db97c
John D'Avanzo <113353460+john-davanzo@users.noreply.github.com>
2023-08-13 11:54:22 -0400

    Create database_queries.sql

---
+++ milestone_3/database_queries.sql
@@ -0,0 +1,11 @@
+-- -----------------------------------------------------
+-- Query: Which movie genres are most popular?
+-- -----------------------------------------------------
+SELECT genre, AVG(popularity) AS average_popularity
+FROM outcomes o
+JOIN movie_genre mg ON o.movie_id = mg.movie_id
+JOIN genres g ON mg.genre_id = g.genre_id
+GROUP BY genre
+ORDER BY AVG(popularity) DESC;
+
+
